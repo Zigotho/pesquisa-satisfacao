a367c3d9313f64a35d499f640083ca84fd5c4372
Matheus de Carvalho Jesus <euescolhoesse@gmail.com>
2022-03-31 20:18:14 -0300

    feat: add survey endpoints

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,11 +10,12 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const survey_module_1 = require("./survey/survey.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [survey_module_1.SurveyModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
---
+++ src/survey/survey.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateSurveyDto } from './dto/create-survey.dto';
+import { UpdateSurveyDto } from './dto/update-survey.dto';
+import { SurveyService } from './survey.service';
+
+@Controller('survey')
+export class SurveyController {
+  constructor(private readonly surveyService: SurveyService) {}
+
+  @Post()
+  create(@Body() createSurveyDto: CreateSurveyDto) {
+    return this.surveyService.create(createSurveyDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.surveyService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.surveyService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateSurveyDto: UpdateSurveyDto) {
+    return this.surveyService.update(+id, updateSurveyDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.surveyService.remove(+id);
+  }
+}
