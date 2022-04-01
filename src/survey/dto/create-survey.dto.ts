f2f23c709bb3098c9cb0475973978e4e8a886406
Matheus de Carvalho Jesus <euescolhoesse@gmail.com>
2022-03-31 21:54:42 -0300

    chore: add validation in survey DTO

--- src/survey/dto/create-survey.dto.ts
+++ src/survey/dto/create-survey.dto.ts
@@ -1,9 +1,19 @@
+import { Prisma } from '@prisma/client';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Survey } from '../entities/survey.entity';
 
 export class CreateSurveyDto extends Survey {
-  id?: number;
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
-  created_at?: string | Date;
+
+  @IsBoolean()
   is_active: boolean;
+
+  @IsOptional()
+  survey_questions?: Prisma.Survey_questionUncheckedCreateNestedManyWithoutSurveyInput;
 }
